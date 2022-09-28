cc90e61d7aacc85e8e7a8ae30614d07ea669279e
Santygr21 <98619680+Santygr21@users.noreply.github.com>
2022-09-27 21:12:36 -0500

    Delete index.js

--- index.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const server = express();
-
-server.use(express.static(__dirname+'/public'))
-
-server.listen(8080, () =>{
-    console.log('server activo');
-})
